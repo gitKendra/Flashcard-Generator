c18bb91360a8fab587ed5066f56c4763b9264c97
Kendra Krzywicki <kendra.krzywicki@knights.ucf.edu>
2017-10-08 18:40:44 -0400

    Added package.json

--- ClozeCard.js
+++ ClozeCard.js
@@ -7,8 +7,7 @@ var ClozeCard = function(text, cloze){
 		this.partial = text.replace(cloze, '...');
 	else{
 		console.log('ERROR: "'+cloze+'" is not contained in "'+text+'"');
-	//	throw 'ERROR: "'+cloze+'" is not contained in "'+text+'"'; 
-	return;
+		return;
 	}
 	this.fullText = text;
 	this.cloze = cloze;
@@ -16,9 +15,3 @@ var ClozeCard = function(text, cloze){
 
 // Export the constructor
 module.exports = ClozeCard;
-
-var card = new ClozeCard("George washington rocks the USA!", "USA");
-
-console.log(card.fullText);
-console.log(card.cloze);
-console.log(card.partial);
